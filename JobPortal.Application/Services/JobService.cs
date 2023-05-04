d8591622a6932b02d2621d6c9ddcbe132a3d8c9d
MarszalekPiotr <marszalekpiotr183@gmail.com>
2023-05-04 08:08:58 +0200

    Dodaj pliki projektów.

---
+++ JobPortal.Application/Services/JobService.cs
@@ -0,0 +1,20 @@
+﻿using JobPortal.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobPortal.Application.Services
+{
+    public class JobService : IJobService
+    {
+        public List<string> GetAllJobs()
+        {
+            return new List<string>();
+        }
+
+        
+
+    }
+}
---
+++ JobPortal.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using JobPortal.Application.Interfaces;
+using JobPortal.Application.Services;
+using JobPortal.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JobPortal.Web.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IJobService _jobService;
+
+        public HomeController(ILogger<HomeController> logger, IJobService jobService)
+        {
+            _logger = logger;
+            _jobService = jobService;
+        }
+
+        [Route("Jobs/All")]
+        public IActionResult Index()
+        {    
+            
+            var listOfJobs = _jobService.GetAllJobs();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
