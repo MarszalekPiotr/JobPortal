5f446ad255726f9351ac53b5d24c5773e7d4af9d
MarszalekPiotr <marszalekpiotr183@gmail.com>
2023-05-10 13:48:06 +0200

    RegisterForCompany

--- JobPortal.Domain/Model/User.cs
+++ JobPortal.Domain/Model/User.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Build.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,21 +13,26 @@ namespace JobPortal.Domain.Model
 
         //wspolne
         //public int Id { get; set; }
-        public string Login { get; set; }
-        public string Password { get; set; }
+        //public string Login { get; set; }
+        //public string Password { get; set; }
 
         
         public string Name { get; set; }
 
-        public string Email { get; set; }
+        //public string Email { get; set; }
 
-        public string PhoneNumber { get; set; }
+     
 
         public string Address { get; set; }
         public bool IsCompany { get; set; }
 
+        public string Description { get; set; }
+
         // optional
+       
         public string? Surname { get; set; }
+        public string? PhoneNumber { get; set; }
+
 
         public virtual ICollection<Application>? Applications { get; set; }
         public virtual ICollection<Job>? Jobs { get; set; }
---
+++ JobPortal.Infrastructure/Migrations/20230510113647_SomeAccountChanges.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobPortal.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SomeAccountChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Login",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "AspNetUsers",
+                newName: "Description");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "AspNetUsers",
+                newName: "Password");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Login",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
